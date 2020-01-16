8755c6e707b34ffd2ebb52d5820ac15d14acc1a6
Ronielson Macedo <ronielsonmacedo@gmail.com>
2020-01-16 20:39:24 -0300

    Render em tela

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+
 /* Classe para controlar a aplicação */
 
 class App {
     constructor() {
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
     }
 
-        /* Registrando Eventos */
+/* Registrando Eventos */
     registerHandlers() {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
@@ -23,8 +25,35 @@ class App {
             html_url:'https://github.com/rocketseat/rocketseat.com.br',
         });
 
-        console.log(this.repositories);
+        this.render(); 
 
+    }
+
+    render() {
+        this.listEl.innerHTML='';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src',repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target','_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        })
     }
 }
 
